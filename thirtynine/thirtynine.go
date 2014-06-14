bf26c0fe62ff98d40d5a98e2045b2d8709c00979
Wesley Moy <wesley@nextdoor.com>
2014-06-13 17:55:04 -0700

    Clean up, add comments

--- thirtynine/thirtynine.go
+++ thirtynine/thirtynine.go
@@ -6,26 +6,33 @@ import (
 )
 
 func main() {
+  // Count is a mapping from a perimeter to the number of integer-sided right triangles that can be formed
   count := make(map[int]int)
+
   for p := 0; p <= 1000; p++ {
     for i := 1; i < p; i++ {
+      // Eliminate duplicate triangles by enforcing that i be the longest side
       for j := i; j < p - i; j++ {
         k := p - i - j
+        // Eliminate duplicate triangles by enforcing that k be the shortest side
         if k < j {
           break
         }
         sq := i * i + j * j
-        sqrt := int(math.Sqrt(float64(sq)))
+        // Round to nearest integer
+        sqrt := int(math.Sqrt(float64(sq)) + 0.5)
+        // Check whether this is an integer square root
         if sqrt * sqrt != sq {
           continue
         }
         if k == sqrt {
           count[p]++
-          //fmt.Println(i, j, k)
         }
       }
     }
   }
+
+  // Creating a new mapping from number of triangles to perimeter
   revcount := make(map[int]int)
   for k, v := range count {
     revcount[v] = k
